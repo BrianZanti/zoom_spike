c52f32c813422c9962074e40f10e42b28511daee
Brian Zanti <brianzanti@gmail.com>
2021-09-14 13:01:16 -0500

    testin

--- app/controllers/zoom_webhooks_controller.rb
+++ app/controllers/zoom_webhooks_controller.rb
@@ -2,5 +2,6 @@ class ZoomWebhooksController < ApplicationController
   def hook
     puts params[:event]
     puts params[:payload]
+    puts "============#{params[:payload][:object][:participant][:user_name]} HAS JOINED A MEETING==========="
   end
 end
